e35e7f61d250ffaf0b5db8970cb5eee9cd3efd6e
Frey Alfredsson <freysteinn@freysteinn.com>
2024-09-12 11:05:32 +0200

    Setup a small TDD example repo
    This project is intended to show how TDD works. It is for a short
    lecture that shows simple JUnit work and tests with Github actions.

---
+++ src/test/java/com/freysteinn/SimpleTest.java
@@ -0,0 +1,15 @@
+package com.freysteinn;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class SimpleTest {
+
+	@Test
+	void sum() {
+		int sum = 2 + 3;
+        assertEquals(5, sum);
+    }
+	
+}
